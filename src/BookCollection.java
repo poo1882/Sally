30b55b6b8a78af929754f15984490437f8ac19c3
DESKTOP-058G3AO\Junior <junior_travian@hotmail.co.th>
2017-11-13 16:13:38 +0700

    bookcollection and sample bookdata

--- src/BookCollection.java
+++ src/BookCollection.java
@@ -1,2 +1,50 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+
 public class BookCollection {
+    private ArrayList<Book> books;
+    private String genre;
+
+
+    BookCollection(String genreType)
+    {
+        genre=genreType;
+    }
+
+    public boolean keepBook(Book newBook)
+    {
+        if(newBook!= null)
+        {
+            books.add(newBook);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteBook(Book target)
+    {
+        int i=0;
+        while(i<books.size())
+        {
+            if(books.get(i)==target)
+            {
+                books.remove(i);
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public void viewAllBook()
+    {
+        int i=0;
+        while (i<books.size())
+        {
+            System.out.println(books.get(i).getName);
+        }
+    }
+
+
+
 }
