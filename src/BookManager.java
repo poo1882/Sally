7979402300b95f9b31ddf4bce238d56026369b89
poo1882 <poo_poo1882@hotmail.com>
2017-11-13 17:16:37 +0700

    BookManager

---
+++ src/BookManager.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class BookManager
+{
+    public static HashMap booksByName;
+    public static HashMap booksByWriter;
+    public static HashMap booksByGenre;
+
+    public BookManager()
+    {
+        
+    }
+
+    public static BookCollection searchByName (String keyword)
+    {
+        
+    }
+
+    public static BookCollection searchByWriter (String keyword)
+    {
+        
+    }
+
+    public static BookCollection searchbyGenre (String Genre)
+    {
+
+    }
+
+    public static void checkOut()
+    {
+        int totalPrice = 0;
+        CUSTOMER.cart.viewAllBooks();
+        System.out.println("Total:  ");
+        for(int i=0;i<CUSTOMER.cart.size();i++)
+        {
+            totalPrice = totalPrice+CUSTOMER.cart.get(i).price;
+        }
+        System.out.print(totalPrice);
+    }
+
+    public static void initializeBooks()
+    {
+
+    }
+
+}
