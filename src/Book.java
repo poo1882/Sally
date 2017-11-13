4571d0fcb524e2414314d853b30762c36fb1ef8a
poo1882 <poo_poo1882@hotmail.com>
2017-11-13 16:22:46 +0700

    src

---
+++ src/Book.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Book
+{
+    private String name;
+    private ArrayList<String> genre;
+    private String writer;
+    private float price;
+
+    public Book (String name, ArrayList<String> genre, String writer, float price, int amount)
+    {
+        this.name = name;
+        this.genre = genre;
+        this.writer = writer;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public void printDetail()
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Genre: ");
+        for (int i=0;i<genre;)
+        {
+            if ( i != 0)
+            {
+                System.out.print(",");
+            }
+            System.out.print(genre.get(i));
+        }
+        System.out.println("Writer: "+writer);
+        System.out.println("Price: "+price); 
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public ArrayList<String> getGenre()
+    {
+        return genre;
+    }
+
+    public float getPrice()
+    {
+        return price;
+    }
+}
